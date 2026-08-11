6823a7faefc499a572972ddd4c79553b14bfd7dd
Kelwin <kelwinwallace01@gmail.com>
2026-08-11 20:43:26 -0300

    feat: implementing complete CRUD and http client tests for the API

--- PersonalBacklog.Api/Controllers/AnimeController.cs
+++ PersonalBacklog.Api/Controllers/AnimeController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PersonalBacklog.Api.Data;
 using PersonalBacklog.Api.Models;
 
 namespace PersonalBacklog.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class AnimeController : ControllerBase
 {
     private readonly BacklogDbContext _context;
@@ -13,5 +14,73 @@ public class AnimeController : ControllerBase
     public AnimeController(BacklogDbContext context)
     {
         _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AnimeCreate([FromBody]Anime anime)
+    {
+        _context.Animes.Add(anime);
+
+        await _context.SaveChangesAsync();
+        
+        return Created($"/api/anime/{anime.Id}", anime);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAnimes()
+    {
+        var animes = await _context.Animes.ToListAsync();
+        
+        return Ok(animes);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAnimeByID(int id)
+    {
+        var anime = await _context.Animes.FindAsync(id);
+        if (anime == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(anime);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> AnimeUpdate(int id, [FromBody] Anime anime)
+    {
+        if (id != anime.Id)
+        {
+            return BadRequest("The ID in the URL does not match the ID in the request body.");
+        }
+        
+        _context.Entry(anime).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Animes.Any(e => e.Id == id))
+                return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> AnimeDelete(int id)
+    {
+        var anime = await _context.Animes.FindAsync(id);
+        if (anime == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Animes.Remove(anime);
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
     }
 }
--- PersonalBacklog.Api/Data/BacklogDbContext.cs
+++ PersonalBacklog.Api/Data/BacklogDbContext.cs
@@ -10,4 +10,24 @@ public class BacklogDbContext : DbContext
     }
     
     public DbSet<Anime> Animes { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<Anime>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateAdded = DateTime.UtcNow;
+                entry.Entity.DateUpdated = DateTime.UtcNow;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.DateUpdated =  DateTime.UtcNow;
+                
+                entry.Property(anime => anime.DateAdded).IsModified = false;
+            }
+                
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
